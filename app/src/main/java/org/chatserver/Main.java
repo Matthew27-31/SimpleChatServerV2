5e57e6104e63dbf9577d222a9c360ede420f2303
Matthew <busfan750@gmail.com>
2025-09-16 18:38:36 -0700

    Intialized revamped chat server

---
+++ app/src/main/java/org/chatserver/Main.java
@@ -0,0 +1,16 @@
+//
+// Version 2 of SimpleChatServer.
+// To be worked on periodically
+// Will be cleaned up in the future.
+//
+// TODO: Clean up the rest of the project and continue the outline
+//
+package org.chatserver;
+
+public class Main {
+    public String getGreeting() {
+        return "Hello World!";
+    }
+
+    public static void main(String[] args) {System.out.println(new Main().getGreeting());}
+}
